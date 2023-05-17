56642febb39dacd7981db477d2225f616841632c
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-05-17 11:05:56 -0500

    Initial Commit

---
+++ TrivaReTheme/TrivaReThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivaReThemeApp.swift
+//  TrivaReTheme
+//
+//  Created by Ashlyn Barrera on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TrivaReThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
